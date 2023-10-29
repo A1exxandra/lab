ecfaf57a39e2b88c4263ea467714f65f4209c0ec
A1exxandra <147980475+A1exxandra@users.noreply.github.com>
2023-10-29 22:59:14 +0300

    Add files via upload

---
+++ Лабораторная работа №3/task1.py
@@ -0,0 +1,13 @@
+def find_index(list_item, find_item_):
+    for index, item in enumerate(items_list):
+        if find_item_ == item:
+            return index
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
